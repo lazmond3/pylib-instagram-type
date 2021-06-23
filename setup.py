84a17adb915f633eee2fb9718aa44b139cbccb2b
lazmond3 <lazmond3@users.noreply.github.com>
2021-06-24 00:51:56 +0900

    update 1.0.3 => 1.0.4

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def load_requires_from_file(filepath):
 
 setup(
     name='lazmond3-pylib-instagram-type',
-    version='1.0.3',
+    version='1.0.4',
     description='update get_multiple_medias',
     long_description=readme,
     author='lazmond3',
